e7411cea801c6f4f9e4dcd96801ac216fff3408f
marykristina4 <mary.ehrstedt@helsinki.fi>
2021-01-27 15:27:10 +0200

    verkkokauppa

---
+++ viikko2/tehtavat5-7/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -0,0 +1,46 @@
+package ohtu.verkkokauppa;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//@Component
+public class Kauppa {
+
+    private Varastoface varasto;
+    private Pankkiface pankki;
+    private Ostoskori ostoskori;
+    private Viiteface viitegeneraattori;
+    private String kaupanTili;
+
+    // @Autowired
+    public Kauppa(Varastoface varastok, Pankkiface pankkik, Viiteface viitek) {
+        varasto = varastok;
+        pankki = pankkik;
+        viitegeneraattori = viitek;
+        kaupanTili = "33333-44455";
+    }
+
+    public void aloitaAsiointi() {
+        ostoskori = new Ostoskori();
+    }
+
+    public void poistaKorista(int id) {
+        Tuote t = varasto.haeTuote(id);
+        varasto.palautaVarastoon(t);
+    }
+
+    public void lisaaKoriin(int id) {
+        if (varasto.saldo(id) > 0) {
+            Tuote t = varasto.haeTuote(id);
+            ostoskori.lisaa(t);
+            varasto.otaVarastosta(t);
+        }
+    }
+
+    public boolean tilimaksu(String nimi, String tiliNumero) {
+        int viite = viitegeneraattori.uusi();
+        int summa = ostoskori.hinta();
+
+        return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
+    }
+
+}
