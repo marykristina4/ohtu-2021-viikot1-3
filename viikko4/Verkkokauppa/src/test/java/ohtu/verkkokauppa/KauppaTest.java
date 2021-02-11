d5645340b01fc5367f2ca493b99b867ecff7a2a5
marykristina4 <mary.ehrstedt@helsinki.fi>
2021-02-11 13:33:14 +0200

    vk4t4

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -170,5 +170,70 @@ public class KauppaTest {
         verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), anyString(), eq(5));
         // toistaiseksi ei v‰litetty kutsussa k‰ytetyist‰ parametreista
     }
+    @Test
+    public void pyydetaanUusiViiteJokaiseenMaksuun() {
+        Pankki mockPankki = mock(Pankki.class);
+        Viitegeneraattori mockViite = mock(Viitegeneraattori.class);
+        
+        Varasto varasto = mock(Varasto.class);
+        // m‰‰ritell‰‰n ett‰ tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        
+        Kauppa kauppa = new Kauppa(varasto, mockPankki, mockViite);
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.tilimaksu("pekka", "11111");
+
+        // tarkistetaan ett√§ t√§ss√§ vaiheessa viitegeneraattorin metodia seuraava()
+        // on kutsuttu kerran
+        verify(mockViite, times(1)).uusi();
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.tilimaksu("pekka", "11111");
+
+        // tarkistetaan ett√§ t√§ss√§ vaiheessa viitegeneraattorin metodia seuraava()
+        // on kutsuttu kaksi kertaa
+        verify(mockViite, times(2)).uusi();
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.tilimaksu("pekka", "11111");
+
+        // tarkistetaan ett√§ t√§ss√§ vaiheessa viitegeneraattorin metodia seuraava()
+        // on kutsuttu kolme kertaa        
+        verify(mockViite, times(3)).uusi();
+    }
+        @Test
+    public void kunAsiointiAloitetaanVanhatTiedotPoissa() {
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // m‰‰ritell‰‰n ett‰ viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        // m‰‰ritell‰‰n ett‰ tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehd‰‰n ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, ett‰ pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), eq(5));
+        // toistaiseksi ei v‰litetty kutsussa k‰ytetyist‰ parametreista
+    }
+
 
 }
