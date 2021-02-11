e8d199a757805c5630004cdec24bc7a5ce41910b
marykristina4 <mary.ehrstedt@helsinki.fi>
2021-02-11 13:45:57 +0200

    vk4t4_vikakintesti

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -234,6 +234,33 @@ public class KauppaTest {
         verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), eq(5));
         // toistaiseksi ei v‰litetty kutsussa k‰ytetyist‰ parametreista
     }
+        @Test
+    public void kunTuotePoistetaanSenHintaPoissa() {
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
 
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // m‰‰ritell‰‰n ett‰ viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        // m‰‰ritell‰‰n ett‰ tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehd‰‰n ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.poistaKorista(1);
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, ett‰ pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), eq(5));
+        // toistaiseksi ei v‰litetty kutsussa k‰ytetyist‰ parametreista
+    }
 
 }
